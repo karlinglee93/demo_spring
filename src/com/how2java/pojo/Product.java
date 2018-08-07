1d2f28867c47305fffca4203c0e4b10b60255d05
karlinglee93 <459607025@qq.com>
2018-08-07 17:52:23 +0800

    injection objects

---
+++ src/com/how2java/pojo/Product.java
@@ -0,0 +1,33 @@
+package com.how2java.pojo;
+
+public class Product {
+
+	private int id;
+	private String name;
+	// 对Product对象，注入一个Category对象
+	private Category category;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+}
